69f1eb4c178fb6c8a1adbf6460d6f3aa80e6dba9
jiveshbhatti <85315081+jiveshbhatti@users.noreply.github.com>
2021-11-21 10:31:28 -0800

    adding a featured section

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,11 +1,14 @@
 import './home.scss'
-import { AcUnit } from '@material-ui/icons'
+
 import { Navbar } from '../components/navbar/Navbar'
+import Featured  from '../components/featured/Featured'
+
+
 export const Home = () => {
     return (
         <div className='home'>
-            <Navbar></Navbar>
-            <img src="https://www.joblo.com/wp-content/uploads/2021/08/dog-movie-poster.jpg" alt="" />
+            <Navbar/>
+            <Featured type='series'/>
         </div>
     )
 }
