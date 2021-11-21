ee1b46b6425baf105960bf73fe1e03bc7d7f6dda
jiveshbhatti <85315081+jiveshbhatti@users.noreply.github.com>
2021-11-21 11:39:09 -0800

    updating list item. fixed no picture bug

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -2,13 +2,16 @@ import './home.scss'
 
 import { Navbar } from '../components/navbar/Navbar'
 import Featured  from '../components/featured/Featured'
-
+import List from '../components/list/List'
 
 export const Home = () => {
     return (
         <div className='home'>
             <Navbar/>
-            <Featured type='series'/>
+            <Featured/>
+            <List/>
+            <List/>
+            <List/>
         </div>
     )
 }
