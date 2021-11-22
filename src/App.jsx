36aeb3afe3b8fff72ef565d5bbc4142bcd6e9695
jiveshbhatti <85315081+jiveshbhatti@users.noreply.github.com>
2021-11-22 11:43:43 -0800

    updated nav bar links and styling. Set up router.

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,28 @@ import Home from "./pages/home/Home";
 import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
 import Login from "./pages/login/Login";
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
 
 const App = () => {
-  return <Home/>;
+  return (
+    <Router>
+    <Switch>
+  <Route exact path='/'>
+      <Home/>
+      </Route>
+      <Route exact path='/movies'>
+        <Home  type='movies'/>
+      </Route>
+      <Route exact path='/series'>
+        <Home  type='series'/>
+      </Route>
+
+      <Route exact path='/watch'>
+        <Watch/>
+      </Route>
+    </Switch>
+    </Router>
+  )
 };
 
 export default App;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import { ArrowDropDown, Notifications, Search } from "@material-ui/icons";
 import { useState } from "react";
 import "./navbar.scss";
-
+import {Link} from 'react-router-dom'
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
 
@@ -17,11 +17,21 @@ const Navbar = () => {
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
             alt=""
           />
+          <Link to='/'className='link'>
           <span>Homepage</span>
+          </Link>
+          <Link to='/series' className='link'> 
           <span>Series</span>
+          </Link>
+          <Link to='/movies'className='link'>
           <span>Movies</span>
+          </Link>
+          <Link to='/'className='link'>
           <span>New and Popular</span>
+          </Link>
+          <Link to='/'className='link'>
           <span>My List</span>
+          </Link>
         </div>
         <div className="right">
           <Search className="icon" />
